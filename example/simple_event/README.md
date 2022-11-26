ae2ccd4e7c4754f3f798a231804163bd0087ad93
Kanji Tanaka <ktanaka@ichain.co.jp>
2022-11-26 13:33:13 +0900

    Fix repository for use

--- example/simple_event/README.md
+++
@@ -1,8 +0,0 @@
-# simple event example
-
-## Start server
-```
-$ cd serverless-s3-local
-$ yarn install
-$ yarn workspace simple-event run start
-```
--- example/simple_event/simple_event.test.js
+++
@@ -1,8 +0,0 @@
-const got = require("got");
-
-it("works with async/await", async () => {
-  const { body } = await got("http://localhost:3000/dev", {
-    responseType: "json",
-  });
-  expect(body).toEqual("ok");
-});
